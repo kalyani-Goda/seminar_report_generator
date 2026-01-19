3a7715ace921fb29e43d9ebbed03d97c5e2f7eab
Nagakalyani Goda <111915034+kalyani-Goda@users.noreply.github.com>
2026-01-19 14:54:44 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,4 +8,4 @@ OLLAMA_CRITIC_MODEL = os.getenv("OLLAMA_CRITIC_MODEL","deepseek-r1")
 
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL","nomic-embed-text")
 
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY","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")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
